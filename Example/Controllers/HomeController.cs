da030f7c8f0dbc5d23a6fef94aea58ddb851c63e
Daniil <33331403+MineLisovich@users.noreply.github.com>
2023-05-19 14:53:49 +0300

    Add project files.

---
+++ Example/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Example.Models;
+
+
+namespace Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            //List<PhoneViewModel> list = new List<PhoneViewModel>();
+            //list.Add(new PhoneViewModel { Id =1, PhoneBrand = "Apple", PhoneModel = "11 pro", Description = "", Price = 0 });
+            //list.Add(new PhoneViewModel { Id = 2, PhoneBrand = "Samsung", PhoneModel = "A52", Description = "", Price = 0 });
+            //list.Add(new PhoneViewModel { Id = 3, PhoneBrand = "Xiaomi", PhoneModel = "Redmi note 10 pro", Description = "", Price = 0 });
+            //list.Add(new PhoneViewModel { Id = 4, PhoneBrand = "LG", PhoneModel = "Wing", Description = "", Price = 0 });
+            //list.Add(new PhoneViewModel { Id = 5, PhoneBrand = "Honor", PhoneModel = "X9a", Description = "", Price = 0 });
+            //PhoneViews = list;
+        }
+       // public List<PhoneViewModel> PhoneViews { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Catalog()
+        {
+            var list = new List<PhoneViewModel>();
+            list.Add(new PhoneViewModel { Id = 1, PhoneBrand = "Apple", PhoneModel = "11 pro", Description = "", Price = 0 });
+            list.Add(new PhoneViewModel { Id = 2, PhoneBrand = "Samsung", PhoneModel = "A52", Description = "", Price = 0 });
+            list.Add(new PhoneViewModel { Id = 3, PhoneBrand = "Xiaomi", PhoneModel = "Redmi note 10 pro", Description = "", Price = 0 });
+            list.Add(new PhoneViewModel { Id = 4, PhoneBrand = "LG", PhoneModel = "Wing", Description = "", Price = 0 });
+            list.Add(new PhoneViewModel { Id = 5, PhoneBrand = "Honor", PhoneModel = "X9a", Description = "", Price = 0 });
+           
+            return View(list);   
+        }
+
+        public IActionResult SinglePage() 
+        {
+            return View();
+        }
+  
+    }
+}
---
+++ Example/Models/PhoneViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Example.Models
+{
+    public class PhoneViewModel
+    {
+        public int Id { get; set; }
+        public string? PhoneBrand { get; set; }
+        public string? PhoneModel { get; set;}
+
+        public string? Description { get; set; }
+
+        public decimal Price { get; set; }
+
+      
+    }
+}
